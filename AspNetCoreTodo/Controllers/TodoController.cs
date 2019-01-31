81e758611ba9641610fef26b92feb7606ace0d4d
Pujan Bajracharya <pujan@swifttech.com>
2019-01-31 17:35:17 +0545

    Manage Users Added

--- AspNetCoreTodo/Controllers/TodoController.cs
+++ AspNetCoreTodo/Controllers/TodoController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreTodo.Controllers
 {
+    //[Authorize]
     public class TodoController : Controller
     {
         private readonly ITodoItemService _todoItemService;
---
+++ AspNetCoreTodo/Extensions/UnAuthorizedAttribute.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace AspNetCoreTodo.Extensions
+{
+    public class UnAuthorizedAttribute : AuthorizeAttribute
+    {
+        //protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        //{
+        //    if (filterContext.HttpContext.User.Identity.IsAuthenticated)
+        //    {
+        //        filterContext.Result = new RedirectResult("/Error/AccessDenied");
+        //    }
+        //    else
+        //    {
+        //        base.HandleUnauthorizedRequest(filterContext);
+        //    }
+        //}
+    }
+}
